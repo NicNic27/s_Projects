4498cebd304c2b4bdfaeeabf951391678be56ff4
nicTawingan <142707950+NicNic27@users.noreply.github.com>
2024-11-05 17:29:46 +0800

    Add files via upload
    Simple console based scientific calculator using C#

---
+++ calculator/ScientificOperations.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace calculator
+{
+    // Responsible for scientific operations
+    public class ScientificOperations
+    {
+        public double SquareRoot(double a) => Math.Sqrt(a);
+        public double Power(double baseNum, double exponent) => Math.Pow(baseNum, exponent);
+        public double Sine(double angle) => Math.Sin(angle);
+        public double Cosine(double angle) => Math.Cos(angle);
+        public double Tangent(double angle) => Math.Tan(angle);
+    }
+}
