93fbb85b11dd9549da769d397fe16b7e822f89d2
nicTawingan <nictawingan@gmail.com>
2024-11-05 23:40:45 +0800

    Posting Portfolio files

---
+++ Portfolio/script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multi-text", {
+    strings: ["Web Developer", " Graphic Animator", "Programmer"],
+    typeSpeed: 50,
+    backSpeed: 50,
+    backDelay: 1000,
+    loop: true
+})
